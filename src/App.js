ec30db0275ee1d3d080842b711035b30d150907c
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-24 21:47:00 -0600

    Add fixtures

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import RecipeList from './RecipeList';
 
 
 function App() {
   
   return (
-    null
+    <RecipeList />
   )
 }
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: "1. Put salt on chicken\n2. Put chicken in the oven\n3. Eat the chicken"
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: "1. Put paprika on pork\n2. Put pork in the oven\n3. Eat the pork"
+  }
+]
 
 export default App;
