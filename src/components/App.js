c42cf3a3f4de5e9b490a96586ad855a11af2f1d4
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-25 21:12:30 -0600

    Import css for recipe list

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import RecipeList from './RecipeList';
+import '../css/app.css';
+
+function App() {
+  return <RecipeList recipes={sampleRecipes} />;
+}
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions:
+      '1. Put salt on chicken\n2. Put chicken in the oven\n3. Eat the chicken',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 Pounds',
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '2 Tbs',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions:
+      '1. Put paprika on pork\n2. Put pork in the oven\n3. Eat the pork',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 Pounds',
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs',
+      },
+    ],
+  },
+];
+
+export default App;
