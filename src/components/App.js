b48053a66820d63e93bbc50b732cd76d22190d74
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-09-25 23:52:24 -0600

    Prop drill handleRecipeDelete function

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,17 @@ function App() {
     setRecipes([...recipes, newRecipe]);
   }
 
-  return <RecipeList recipes={recipes} handleRecipeAdd={handleRecipeAdd} />;
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
+  return (
+    <RecipeList
+      recipes={recipes}
+      handleRecipeAdd={handleRecipeAdd}
+      handleRecipeDelete={handleRecipeDelete}
+    />
+  );
 }
 
 const sampleRecipes = [
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,14 +2,24 @@ import React from 'react';
 import IngredientList from './IngredientList';
 
 export default function Recipe(props) {
-  const { name, cookTime, servings, instructions, ingredients } = props;
+  const {
+    id,
+    name,
+    cookTime,
+    servings,
+    instructions,
+    ingredients,
+    handleRecipeDelete,
+  } = props;
   return (
     <div className="recipe">
       <div className="recipe__header">
         <h3 className="recipe__title">{name}</h3>
-        <div >
+        <div>
           <button className="btn btn--primary mr-1">Edit</button>
-          <button className="btn btn--danger">Delete</button>
+          <button className="btn btn--danger" onClick={() => handleRecipeDelete(id)}>
+            Delete
+          </button>
         </div>
       </div>
       <div className="recipe__row">
@@ -22,13 +32,15 @@ export default function Recipe(props) {
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Instructions</span>
-        <div className="recipe__value recipe__instructions recipe__value--indented">{instructions}</div>
+        <div className="recipe__value recipe__instructions recipe__value--indented">
+          {instructions}
+        </div>
       </div>
       <div className="recipe__row">
         <span className="recipe__label">Ingredients</span>
         <div className="recipe__value recipe__value--indented">
           <IngredientList ingredients={ingredients} />
-          </div>
+        </div>
       </div>
     </div>
   );
